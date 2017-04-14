81ba549198c96e285ff1d072341a17ffd6b74f1a
MK <mahmoud.k.nasr@gmail.com>
2017-04-14 05:30:30 +0200

    Create the PWSO model and migration with basic sign in columns

---
+++ database/migrations/2017_04_14_032548_create_p_w_s_os_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePWSOsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /*
+         * @TODO
+         * complete the PWSO table
+         */
+        Schema::create('p_w_s_os', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('name',40);
+            $table->string('username',20);
+            $table->string('email',20);
+            $table->string('password',100);
+
+            $table->unique(['username','email']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('p_w_s_os');
+    }
+}
