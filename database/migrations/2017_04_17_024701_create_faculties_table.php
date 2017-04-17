3896b184985a28d4e33225ce887c946728db2d85
MK <mahmoud.k.nasr@gmail.com>
2017-04-17 20:01:33 +0200

    polish the seeders and migrations

--- database/migrations/2017_04_17_024701_create_faculties_table.php
+++ database/migrations/2017_04_17_024701_create_faculties_table.php
@@ -18,16 +18,17 @@ class CreateFacultiesTable extends Migration
             $table->unsignedInteger('university_id');
             $table->timestamps();
             $table->string('fname',40);
-            $table->string('fpresident_name',30);
-            $table->string('fpast_presidents',255);
-            $table->string('furl',255);
-            $table->string('ffb_url',255);
-            $table->string('fcontacts',30);
+            $table->string('fwebsite',255);
+            $table->string('ffacebook_page',255);
             $table->string('fdescription',255);
             $table->string('departments',255);
-            $table->longText('f_logo');
+            $table->longText('flogo');
             $table->double('fx',20,15);
             $table->double('fy',20,15);
+
+            $table->string('fpresident_name',30);
+            $table->string('fpast_presidents',255);
+            $table->string('fcontacts',30);
         });
 
         Schema::table('faculties', function(Blueprint $table) {
